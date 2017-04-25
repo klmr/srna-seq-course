2be70aa32fb7b7c9153879dbfa32e6bc88da99c9
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-04-25 16:52:39 +0100

    Remove dependency on dplyr

--- src/peterplot.r
+++ src/peterplot.r
@@ -6,22 +6,14 @@ infile = args[2]
 outfile = args[3]
 
 library(Biostrings)
-library(dplyr)
-library(tibble)
 library(ggplot2)
 
 sequences = readDNAStringSet(infile)
 
-srna_freq = tibble(Length = width(sequences),
-                   Nucleotide = substr(sequences, 1, 1)) %>%
-    filter(Nucleotide != 'N') %>%
-    group_by(Length, Nucleotide) %>%
-    summarize(N = n()) %>%
-    ungroup() %>%
-    mutate(Proportion = N / sum(N))
+srna_freq = data.frame(Length = width(sequences),
+                       Nucleotide = substr(sequences, 1, 1))
 
 ggplot(srna_freq) +
-    aes(Length, Proportion, fill = Nucleotide) +
-    geom_bar(stat = 'identity') +
-    scale_x_continuous(limits = c(18, 46)) +
+    aes(Length, fill = Nucleotide) +
+    geom_bar(aes(y = ..count.. / sum(..count..))) +
     ggtitle(name)
